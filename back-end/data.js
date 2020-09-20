5ff72430f1d81cbe48da68796c30ebf7555079e2
Dhanya-krishnan2 <dhanyakunhi@gmail.com>
2020-09-20 18:01:06 +0800

    added basic functionality

---
+++ back-end/data.js
@@ -0,0 +1,46 @@
+export default {
+  products: [
+    {
+      _id: '1',
+      name: 'Books',
+      category: 'Engineeering',
+      image: '/images/d1.jpg',
+      price: 150,
+      brand: ' orignal print',
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 6,
+    },
+    {
+      _id: '2',
+      name: 'Books',
+      category: 'Fiction',
+      image: '/images/d1.jpg',
+      price: 150,
+      brand: ' limited edition',
+      rating: 4.2,
+      numReviews: 5
+    },
+    {
+      _id: '3',
+      name: 'Books',
+      category: 'Educational Book',
+      image: '/images/d1.jpg',
+      price: 170,
+      brand: ' original brand',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 6,
+    }, {
+      _id: '4',
+      name: ' Limited Edition',
+      category: 'Pants',
+      image: '/images/d1.jpg',
+      price: 70,
+      brand: ' International',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 6,
+    },
+  ]
+}
---
+++ front-end/src/data.js
@@ -0,0 +1,43 @@
+export default {
+  products: [
+    {
+      _id: '1',
+      name: 'electrical engineering',
+      category: 'books',
+      image: '/images/d1.jpg',
+      price: 60,
+      brand: ' International',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      _id: '2',
+      name: 'computer science engineering',
+      category: 'books',
+      image: '/images/d1.jpg',
+      price: 50,
+      brand: ' International',
+      rating: 4.2,
+      numReviews: 5
+    },
+    {
+      _id: '3',
+      name: 'Information technology',
+      category: 'books',
+      image: '/images/d1.jpg',
+      price: 70,
+      brand: ' International',
+      rating: 4.5,
+      numReviews: 8
+    }, {
+      _id: '4',
+      name: 'Electronics engineering',
+      category: 'books',
+      image: '/images/d1.jpg',
+      price: 70,
+      brand: ' International',
+      rating: 4.5,
+      numReviews: 8
+    },
+  ]
+}
